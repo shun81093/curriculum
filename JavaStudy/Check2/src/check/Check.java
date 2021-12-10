a180e58684148982fab4d02d38f53c37cdb7a244
shun81093 <shun81093@gmail.com>
2021-12-11 01:04:10 +0900

    update check2

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,10 +1,20 @@
 package check;
 
 public class Check {
+	
+	private static String firstName = "谷口";
+	private static String lastName = "駿一";
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		
+		Pet Pet = new Pet(firstName, lastName);
+		printName(Pet.getName(), Pet.getMasterName());
+		
 	}
+	
+	private static void printName(final String firstName, final String masterName) {
+		System.out.println("printNameメソッド → " + firstName + masterName);			
+	}
+	
 
 }
