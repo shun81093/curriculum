52ae67e1a7e32d6698fd1eee3e1461c0a0c1c412
shun81093 <93690291+shun81093@users.noreply.github.com>
2021-12-11 01:23:34 +0900

    Update Check2

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,5 +1,7 @@
 package check;
 
+import constants.Constants;
+
 public class Check {
 	
 	private static String firstName = "谷口";
@@ -7,14 +9,26 @@ public class Check {
 
 	public static void main(String[] args) {
 		
-		Pet Pet = new Pet(firstName, lastName);
-		printName(Pet.getName(), Pet.getMasterName());
+		printName(firstName, lastName);
 		
+		Pet Pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		Pet.introduce();
+		
+		RobotPet RobotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		RobotPet.introduce();
 	}
 	
-	private static void printName(final String firstName, final String masterName) {
+	private static void printName(String firstName, String masterName) {
 		System.out.println("printNameメソッド → " + firstName + masterName);			
 	}
 	
 
 }
+
+/* 
+ * printNameメソッド　→　谷口駿一
+ * ■僕の名前はjava吉です
+ * ■ご主人様はhogeです
+ * ◇私はロボット。名前はR202。
+ * ◇ご主人様はルーク。
+ */
